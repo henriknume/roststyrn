c81e92642d8e1c156daec428605237f64c4b0f50
davve94 <fodavid@student.chalmers.se>
2016-02-02 09:32:44 +0100

    Simulator design

---
+++ simulator_form.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+
+    Private Sub ProgressBar1_Click(sender As Object, e As EventArgs) Handles ProgressBar1.Click
+
+    End Sub
+
+    Private Sub ProgressBar2_Click(sender As Object, e As EventArgs) Handles ProgressBar2.Click
+
+    End Sub
+
+    Private Sub ProgressBar3_Click(sender As Object, e As EventArgs) Handles ProgressBar3.Click
+
+    End Sub
+
+    Private Sub ProgressBar4_Click(sender As Object, e As EventArgs) Handles ProgressBar4.Click
+
+    End Sub
+End Class
