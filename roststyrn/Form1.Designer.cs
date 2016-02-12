7004e498ea4375d6da2926b92815839627989f10
johnss0n <johnsson3.14@gmail.com>
2016-02-12 12:43:38 +0100

    Bugfix
    It now runs Form1_Load

--- roststyrn/Form1.Designer.cs
+++ roststyrn/Form1.Designer.cs
@@ -88,6 +88,7 @@
             this.KeyPreview = true;
             this.Name = "Form1";
             this.Text = "VoiceControl";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
