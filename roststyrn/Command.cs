bfde82264d99f33eeaac55014adc83f128c97441
henriknume <henriknume@gmail.com>
2016-02-17 13:14:36 +0100

    Added VoiceCommand, ad Command classes
    VoiceCommand should contain all availible commands, read either from a
    file or the database.
    
    The Command class is extended to Lamp- Axle and Stop-commands, Feel free
    to change whatever is needed :)

---
+++ roststyrn/Command.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace roststyrn
+{
+    abstract class Command
+    {
+
+        abstract public void Send();
+    }
+
+    class AxleCommand : Command
+    {
+        private int ID;
+        private int pos;
+        private int speed;
+        public AxleCommand(int ID , int pos, int speed)
+        {
+            this.ID = ID;
+            this.pos = pos;
+            this.speed = speed;
+        }
+        public override void Send()
+        {
+            //sim.SendAxleMoveCommand(ID, pos, speed);
+            Console.WriteLine("SendAxleMoveCommand("+ ID+ ", "+pos+", "+speed+")");
+        }
+    }
+
+    class StopCommand : Command
+    {
+        private int ID;
+        public StopCommand(int ID)
+        {
+            this.ID = ID;
+        }
+        public override void Send()
+        {
+            //sim.SendAxleStopCommand(ID);
+            Console.WriteLine("SendAxleStopCommand(" + ID + ")");
+        }
+    }
+
+    class LampCommand : Command
+    {
+        private int ID;
+        private int mVDC;
+        public LampCommand(int ID, int mVDC)
+        {
+            this.ID = ID;
+            this.mVDC = mVDC;
+        }
+        public override void Send()
+        {
+            //sim.SendAxleMoveCommand(ID, pos, speed);
+            Console.WriteLine("SendLampCommand(" + ID + ", "+ mVDC+")");
+        }
+    }
+
+}
---
+++ roststyrn/VoiceCommands.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace roststyrn
+{
+    public class VoiceCommands
+    {
+
+        static Dictionary<string, List<Command>> allCommands = new Dictionary<string, List<Command>>();
+
+
+        public void Init(string lang)
+        {
+            //read all commands from database or file, and put them in allCommands
+        }
+
+        public static bool Contains(string key)
+        {
+            if (allCommands.ContainsKey(key))
+            {
+                return true;
+            }
+            return false;
+        }
+       
+        public static Command getCommand(string s)
+        {
+            // return command with the correct key
+
+            return new AxleCommand(1, 70, 100);
+        }
+
+        public static void addCommand(string key)
+        {
+
+        }
+    }
+}
+
+//getcommand().send()
