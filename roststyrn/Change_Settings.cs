f7eb656f1f60419ec3dacea5124ffd7016676024
davve94 <fodavid@student.chalmers.se>
2016-02-22 13:12:27 +0100

    added customize files and change settings files

--- roststyrn/Change_Settings.cs
+++ roststyrn/Change_Settings.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Collections;
 
-namespace Customize
+namespace roststyrn
 {
     public partial class Change_Setting : Form
     {
