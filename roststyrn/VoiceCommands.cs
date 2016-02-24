d5a60f49c52e649a2fee80466667d45ad77e1927
JonasArvidsson1 <jonas.arvidsson0@gmail.com>
2016-02-24 15:33:26 +0100

    Cleaned up code

--- roststyrn/VoiceCommands.cs
+++ roststyrn/VoiceCommands.cs
@@ -74,10 +74,10 @@ namespace roststyrn
             {
                 command = new LampCommand(Int32.Parse(args[1]), Int32.Parse(args[2]));
             }
-            else if (args[0] == "OpenProgramCommand")
-            {
-                command = new OpenProgramCommand((args[1]));
-            }
+         //   else if (args[0] == "OpenProgramCommand")
+           // {
+             //   command = new OpenProgramCommand((args[1]));
+           // }
             else if (args[0] == "WowFactorCommand")
             {
                 command = new WowFactorCommand((args[1]));
