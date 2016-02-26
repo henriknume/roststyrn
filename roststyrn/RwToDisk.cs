75b3e892f0d89193a8d2f95b851047b830791d28
johnss0n <johnsson3.14@gmail.com>
2016-02-26 16:38:13 +0100

    Added RwToDisk.cs
    * Read/Write object to disk.

---
+++ roststyrn/RwToDisk.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+public class RwToDisk
+{
+    // Writing to disk:
+    // WriteToBinaryFile<SomeClass>("filename.dat", object1);
+    // Path can be included with filename eg. "C:/filename.txt"
+    public static void Write<T>(String fileName, T obj)
+	{
+        using (Stream stream = File.Open(fileName, FileMode.Create))
+        {
+            var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+            binaryFormatter.Serialize(stream, obj);
+        }
+    }
+
+    // Read from disk:
+    // SomeClass object1 = Read<SomeClass>("filename.dat");
+    public static T Read<T>(String fileName)
+    {
+        using (Stream stream = File.Open(fileName, FileMode.Open))
+        {
+            var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+            return (T)binaryFormatter.Deserialize(stream);
+        }
+    }
+}
